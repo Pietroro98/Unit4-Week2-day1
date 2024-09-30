25c2fde288cfad87b6810aa29224dcbb8768ab98
Pietroro98 <pietroro98@gmail.com>
2024-09-30 16:20:21 +0200

    Unit4-Week2-day1

--- src/Esercizio1/Main.java
+++ src/Esercizio1/Main.java
@@ -9,20 +9,20 @@ public class Main {
         Random random = new Random();
         int[] array = new int[5];
 
-        // Riempie l'array con valori casuali tra 1 e 10
+
         for (int i = 0; i < array.length; i++) {
             array[i] = 1 + random.nextInt(10);
         }
 
         while (true) {
-            // Stampa l'array corrente
+
             System.out.print("Array corrente: ");
             for (int num : array) {
                 System.out.print(num + " ");
             }
             System.out.println();
 
-            // Chiede all'utente di inserire il valore e la posizione
+
             System.out.print("Inserisci il valore (0 per terminare): ");
             int value = scanner.nextInt();
             if (value == 0) {
